1ce37cd95cf5f2087408f0a15590673a679a4d59
arham2003 <arhamkx@gmail.com>
2024-12-09 16:28:01 +0500

    2nd deployment try on vercel

--- Server Side/utils/db.js
+++ Server Side/utils/db.js
@@ -1,16 +1,30 @@
-import mysql from 'mysql2';
+import { MongoClient } from 'mongodb';
 import 'dotenv/config';
 
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE} `
+// MongoDB connection string
+const mongoUri = `mongodb+srv://${import.meta.env.VITE_MONGOUSER}:${import.meta.env.VITE_MONGOPASSWORD}@${import.meta.env.VITE_MONGOHOST}/?retryWrites=true&w=majority&appName=Cluster1`;
 
-const con = mysql.createConnection(urlDB);
+let con; // Database instance
 
-con.connect(function(err) {
-    if (err) {
-        console.error("Connection error: ", err);
-    } else {
-        console.log("Connected");
+async function connectDB() {
+    if (!con) {
+        try {
+            // Create a new MongoClient instance
+            const client = new MongoClient(mongoUri);
+            
+            // Connect to the MongoDB server
+            await client.connect();
+            console.log("Connected to MongoDB");
+            
+            // Select the database (replace `mydatabase` with your actual database name)
+            con = client.db(import.meta.env.VITE_MONGODATABASE);
+        } catch (err) {
+            console.error("Connection error: ", err);
+            throw err;
+        }
     }
-});
+    return con;
+}
 
-export default con;
+// Export the database connection as 'con'
+export default connectDB();
